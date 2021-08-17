248cb783a93348e0c6d58097e154dfa498c73908
Giancarlo <castag.gian@gmail.com>
2021-08-16 20:16:08 -0500

    login UI adjustment

--- Toolbox+Coord/ViewController/Login/LoginViewController.swift
+++ Toolbox+Coord/ViewController/Login/LoginViewController.swift
@@ -8,17 +8,38 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Kingfisher
 
 class LoginViewController: UIViewController, StoryboardInitializable {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var loginButton: UIButton!
     var viewModel: LoginViewModel!
     private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
+        setBackgroundImage()
         setUpBinds()
         // Do any additional setup after loading the view.
+    }
+    
+    func setBackgroundImage() {
+        guard let url = URL(string: "http://placeimg.com/320/480/any") else { return }
+        let processor = DownsamplingImageProcessor(size: backgroundImage.bounds.size)
+                     |> RoundCornerImageProcessor(cornerRadius: 0)
+        backgroundImage.kf.setImage(
+            with: url,
+            placeholder: nil,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(0.1)),
+                .cacheOriginalImage,
+                .memoryCacheExpiration(.expired),
+                .diskCacheExpiration(.expired)
+            ])
     }
 
 }
